0ffed09a9b118e9b0d53a7f5e08d714cdf8f3727
The Game Changer <146466925+2thegamechanger@users.noreply.github.com>
2023-09-30 10:34:56 +0200

    added input maps for arrow, wasd, d-pad and basic movement

--- scripts/ship.gd
+++ scripts/ship.gd
@@ -7,10 +7,17 @@ func _ready():
 		
 
 func _physics_process(delta):
-	var vel = compute_movement(delta)
-	move_and_slide(vel)
-	_direction = vel.normalized()
+	move_and_slide(compute_movement(delta))
 
 
 func compute_movement(delta):
-	pass
+	var sp = Settings.PLAYER_SPEED
+	var vel = Vector2.ZERO
+	if Input.is_action_pressed("left"): vel += Vector2.LEFT
+	if Input.is_action_pressed("right"): vel += Vector2.RIGHT
+	if Input.is_action_pressed("up"): vel += Vector2.UP
+	if Input.is_action_pressed("down"): vel += Vector2.DOWN
+	
+	_direction = vel.normalized()
+	print(_direction * sp)
+	return _direction * sp #*delta
--- settings.gd
+++ settings.gd
@@ -1,3 +1,6 @@
+extends Node
 # this is a preloaded file containing all settings for the game
 
 const MY_SETTING = "very nice"
+
+const PLAYER_SPEED = 300
