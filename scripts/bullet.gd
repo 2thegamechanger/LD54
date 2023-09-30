f84d88dfc5828a0ceeacf5c1a5eff79cfa58e305
The Game Changer <146466925+2thegamechanger@users.noreply.github.com>
2023-09-30 14:06:38 +0200

    set up collision shapes

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -7,18 +7,22 @@ export(Vector2) var direction = Vector2.LEFT
 onready var core = $core
 onready var motion = $motion
 var _direction = Vector2.RIGHT
-var _velocity = Vector2.ZERO
+var _current_direction = Vector2.RIGHT
+var _current_velocity = Vector2.ZERO
 
 func _ready():
 	_direction = direction.normalized()
-	_velocity = _direction * velocity
+	_current_velocity = _direction * velocity
 
 func _physics_process(delta):
 	var vec = move_and_slide(compute_movement())
 
 func compute_movement():
-	_direction = _velocity.normalized()
+	_direction = _current_velocity.normalized()
 	#_velocity = _direction * velocity
 	motion.rotation = atan2(_direction.x, -_direction.y)
 	
-	return _velocity
+	return _current_velocity
+	
+func change_direction(phi:float):
+	_current_velocity = _direction.rotated(phi) * velocity
